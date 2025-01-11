ea52908c187e2ac6d3ae75d4a1552a0e0f1d8a20
Jitendra120901 <jitendrachoudhary2729@gmail.com>
2025-01-11 15:28:29 +0530

    updated db connection

--- dataconnection.js
+++ dataconnection.js
@@ -1,29 +1,28 @@
-const mongoose= require("mongoose");
-const DB='mongodb+srv://jitendra_12:<db_password>@cluster0.83h76ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.set('strictQuery', true);
-mongoose.connect(DB,{
- useNewUrlParser:true,
-    
-}).catch(()=>{
-    console.log('connected  not');
-   
-}).then((app_login)=>{
-  
-    console.log(' connected');
-})
+const mongoose = require("mongoose");
+const DB =
+  "mongodb+srv://jitendra_12:<db_password>@cluster0.83h76ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+  })
+  .catch(() => {
+    console.log("connected  not");
+  })
+  .then((app_login) => {
+    console.log(" connected");
+  });
 
-const app_login_Schema= new mongoose.Schema({
-
-    name:{type:String },
-    email:{ type:String },
-    pass:{ type:String},
-    repass:{type:String},
-   image :{ type:String}
-    
-    
-})
+const app_login_Schema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  pass: { type: String },
+  repass: { type: String },
+  image: { type: String },
+});
 
 const login_data = mongoose.model("App_login", app_login_Schema);
 
-module.exports =mongoose.model('info' , app_login_Schema);
- 
+module.exports = mongoose.model("info", app_login_Schema);
